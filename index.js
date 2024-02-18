ff5eccce7bc68d15be18efa69049bf6ae8e74525
ACHRAF MABSOUT <mabsoutachraf1999@gmail.com>
2024-02-18 15:21:58 +0100

    add get all stagiaire route

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const mongoose = require("mongoose");
+
+// import stagiaire model
+
+const Stagiaire = require('./models/stagiaire');
 
 const app = express();
+
+// connet to db
+mongoose.connect("mongodb://localhost:27017/mydb2000");
+
+
+// get all stagiaires
+app.get("/stagiaires", async (req, res) => {
+    
+    const stagiaires = await Stagiaire.find();
+
+    res.json(stagiaires);
+});
+
 
 
 
